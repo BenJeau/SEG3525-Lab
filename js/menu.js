ffdbb8ccc81ecfca112cd688cdc0ecb17dfdb6c6
Diedrick Ng <dngen049@uottawa.ca>
2019-05-28 15:26:10 -0400

    Made the menu page more dynamic

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,6 @@
+
+
+
 $(document).ready(() =>{
     var url = window.location.href;
     var param = url.split("#")[1];
@@ -5,15 +8,16 @@ $(document).ready(() =>{
     $.getJSON("../data/menu.json", (data) => {
 		data.forEach((val, key) => {
 			if(val.restaurant == param){
+                var temp = val.id;
                 $("#plates-infos").append(
                     `<div class="plate">
-                            <img  src="${val.img}" alt="yougurt">
+                            <img  src="${val.img}" >
                             <div class="plate-info">
                                 <div style="width:50%; padding-left:10px; text-align: left">
                                     <h4>${val.id}</h4>
                                     <h4>${val.price}$</h4>
                                 </div>
-                                <input type="image" src="../img/icons/add-shopping-cart.svg" width="35" height="35" style="margin-left:35%; margin-top:7%" >
+                                <input  type="image" onClick="Add(this)" name="${val.id}" value="${val.price}" id="cartBtn" name="butoon"src="../img/icons/add-shopping-cart.svg" width="35" height="35" style="margin-left:35%; margin-top:7%" >
                                 
                             </div>
                     </div>`
@@ -21,5 +25,20 @@ $(document).ready(() =>{
 
             }
 		});
-	});
+    });
+
+    
+    
+   
+   
 });
+
+function Add(o){
+    var product = {
+        "name": o.name,
+        "price":o.value
+    };
+    console.log(product);
+}
+
+
