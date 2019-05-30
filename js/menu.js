818d792e06bb9d8fa62080c7d88dea5f65d43684
Benoît Jeaurond <bjeau025@uottawa.ca>
2019-05-30 13:18:53 -0400

    Updated data format and number of items in the cart

--- js/menu.js
+++ js/menu.js
@@ -1,37 +1,43 @@
-
-
-
 $(document).ready(() =>{
+	updateCartNum();
     var url = window.location.href;
     var param = url.split("#")[1];
 
     $.getJSON("../data/menu.json", (data) => {
-		data.forEach((val, key) => {
-			if(val.restaurant == param){
-                var temp = val.id;
-                $("#plates-infos").append(
-                    `<div class="plate">
-                            <img  src="${val.img}" >
-                            <div class="plate-info">
-                                <div style="width:50%; padding-left:10px; text-align: left">
-                                    <h4>${val.id}</h4>
-                                    <h4>${val.price}$</h4>
-                                </div>
-                                <input  type="image" onClick="Add(this)" name="${val.id}" value="${val.price}" id="cartBtn" name="butoon"src="../img/icons/add-shopping-cart.svg" width="35" height="35" style="margin-left:35%; margin-top:7%" >
-                                
-                            </div>
-                    </div>`
-                );
-
-            }
+		data[param].forEach((val, key) => {
+			var temp = val.id;
+			$("#plates-infos").append(
+				`<div class="plate">
+						<img  src="${val.img}" >
+						<div class="plate-info">
+							<div style="width:50%; padding-left:10px; text-align: left">
+								<h4>${val.id}</h4>
+								<h4>${val.price}$</h4>
+							</div>
+							<input  type="image" onClick="Add(this)" name="${val.id}" value="${val.price}" id="cartBtn" name="butoon"src="../img/icons/add-shopping-cart.svg" width="35" height="35" style="margin-left:35%; margin-top:7%" >
+							
+						</div>
+				</div>`
+			);
 		});
-    });
+	});
 
-    
-    
-   
-   
+	window.addEventListener("storage", updateCartNum);
 });
+
+const updateCartNum = () => {
+	let items = localStorage.getItem("menuItemIndex");
+	let numItems = "";
+
+	if (items) {
+		numItems = JSON.parse(localStorage.getItem("menuItemIndex")).length;
+	}
+
+	if (numItems == 0) $("#number").css("opacity", 0);
+	else if ($("#number").css("opacity") == 0) $("#number").css("opacity", 1);
+
+	$("#number p").html(numItems);
+}
 
 function Add(o){
     var item = {
@@ -49,6 +55,7 @@ function Add(o){
     
    
    localStorage.setItem("menuItemIndex", JSON.stringify(menuItemIndex));
+   updateCartNum();
 }
 
 
