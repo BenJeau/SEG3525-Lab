3c06f6c977c0fe9ca2ebaef988c933cde9796f28
Benoît Jeaurond <bjeau025@uottawa.ca>
2019-06-17 07:24:48 -0400

    Added paper and icons

--- restaurant/index.js
+++ restaurant/index.js
@@ -1,17 +1,20 @@
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import React from 'react';
 import App from './src/components/App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './src/redux/store/index';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 export default function Main() {
 	return (
 		<Provider store={store}>
 			<PersistGate loading={null}
 				persistor={persistor}>
+				<PaperProvider>
 					<App />
+				</PaperProvider>
 			</PersistGate>
 		</Provider>
 	);
