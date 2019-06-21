29a0ea3c6eebd5ef8b120f64460a39e366e3b072
Benoît Jeaurond <bjeau025@uottawa.ca>
2019-06-21 10:41:07 -0400

    Fixed padding issue

--- restaurant/src/components/Menu.js
+++ restaurant/src/components/Menu.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
 import { Button } from 'react-native-paper';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { Header } from 'react-navigation';
 
 class Menu extends React.PureComponent {
 	render() {
-		return(
+		return (
 			<View style={styles.container}>
-				<Button mode="contained" onPress={() => this.props.navigation.navigate("Commande")}>
-					Next screen
-				</Button>
+				<ScrollView style={styles.content}>
+					<Button mode="contained" onPress={() => this.props.navigation.navigate("Commande")}>
+						Next screen
+					</Button>
+				</ScrollView>
 			</View>
 		);
 	}
@@ -19,7 +23,11 @@ export default connect()(Menu);
 
 const styles = StyleSheet.create({
 	container: {
-	  flex: 1,
-	  padding: 20,
+		flex: 1,
 	},
+	content: {
+		flex: 1,
+		padding: 20,
+		paddingTop: 10 + getStatusBarHeight() + Header.HEIGHT,
+	}
 });
