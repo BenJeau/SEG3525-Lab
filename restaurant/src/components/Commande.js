e9a74f1d31e12430e1978e1e09c832c0b2b9151f
Benoît Jeaurond <bjeau025@uottawa.ca>
2019-06-21 10:43:15 -0400

    Fixed padding issue

--- restaurant/src/components/Commande.js
+++ restaurant/src/components/Commande.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
 import { Button } from 'react-native-paper';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { Header } from 'react-navigation';
 
 class Commande extends React.PureComponent {
 	render() {
 		return(
 			<View style={styles.container}>
+			<ScrollView style={styles.content}>
 				<Button mode="contained" onPress={() => this.props.navigation.navigate("Paiement")}>
 					Next screen
 				</Button>
+				</ScrollView>
 			</View>
 		);
 	}
@@ -19,7 +23,11 @@ export default connect()(Commande);
 
 const styles = StyleSheet.create({
 	container: {
-	  flex: 1,
-	  padding: 20,
+		flex: 1,
 	},
+	content: {
+		flex: 1,
+		padding: 20,
+		paddingTop: 10 + getStatusBarHeight() + Header.HEIGHT,
+	}
 });
